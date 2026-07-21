080c3b831b16bcdfc65c25ce983a71fb9dcaf6af
Anushka <anushkagupta200615@gmail.com>
2026-07-22 03:25:42 +0530

    Solve global-avg-pooling

--- global-avg-pooling/global-avg-pooling.py
+++ global-avg-pooling/global-avg-pooling.py
@@ -1,11 +1,16 @@
 import numpy as np
 
 def global_avg_pool(x):
-    if x.ndim == 3:
+   
+    x = np.asarray(x, dtype=float)
+
+    if x.ndim == 3:  # (C,H,W)
         C, H, W = x.shape
-        return x.reshape(C, H * W).mean(axis=1).astype(float)
-    elif x.ndim == 4:
+        return x.reshape(C, H*W).mean(axis=1).astype(float)
+
+    elif x.ndim == 4:  # (N,C,H,W)
         N, C, H, W = x.shape
-        return x.reshape(N, C, H * W).mean(axis=2).astype(float)
+        return x.reshape(N, C, H*W).mean(axis=2).astype(float)
+
     else:
         raise ValueError("Input must have shape (C,H,W) or (N,C,H,W)")
