ddfa91f12568ef272e458cdb9f57168bd176e667
Anushka <anushkagupta200615@gmail.com>
2026-07-22 03:21:52 +0530

    Solve causal-masking

--- causal-masking/causal-masking.py
+++ causal-masking/causal-masking.py
@@ -1,8 +1,14 @@
 import numpy as np
 
 def apply_causal_mask(scores, mask_value=-1e9):
+  
+    scores = np.asarray(scores, dtype=float)
     T = scores.shape[-1]
-    mask = np.triu(np.ones((T, T)), k=1).astype(bool)
-    masked_scores = scores.copy().astype(float)
-    masked_scores[..., mask] = mask_value
+
+  
+    mask = np.triu(np.ones((T, T), dtype=bool), k=1)
+
+   
+    masked_scores = np.where(mask, mask_value, scores)
+
     return masked_scores
